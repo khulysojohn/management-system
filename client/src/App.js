6ea44d94c415a2e2595a1efc50042381fe15d690
khulyso <khulyso.dev@gmail.com>
2023-05-09 19:16:41 +0200

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,30 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   
+        <div className="head">
+          <h1>Management system</h1>
+        </div>
+        
+        <div className="form">
+          <label>fullnames:</label>
+          <input type="text"/>
+          <label>surname:</label>
+          <input type="text"/>
+          <label>email:</label>
+          <input type="text"/>
+          <label>age:</label>
+          <input type="number"/>
+          <labe>class:</labe>
+          <input type="text"/>
+          <label>gender:</label>
+          <input type="text"/>
+        </div>
+        <div className="button">
+          <button>Add student</button>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <button>Show student</button>
+        </div>
+        <hr/>
     </div>
   );
 }
